15c62c4e4e5e360d849f001f0ed434ff39e58284
Marllon Zucolotto <marlonzucolotto@gmail.com>
2021-12-11 17:00:41 -0300

    1º

---
+++ dados_pessoa.dart
@@ -0,0 +1,8 @@
+void main() {
+  //Leia o nome de uma pessoa, a sua idade e o seu salário, e mostre essas informações.
+  String person = "Marllon";
+  int age = 21;
+  double salary = 3.658;
+//usando o '\' antes do '$' consigo imprimir esse mesmo símbolo para o usuario.
+  print('$person tem $age e ganha \$$salary.');
+}
---
+++ troca_valores.dart
@@ -0,0 +1,16 @@
+void main() {
+  /*Duas variáveis (A e B) possuem valores distintos( A = 5 e B = 10). Crie um algoritmo que
+armazene estes dois valores nessas duas variáveis, e efetue a troca dos valores de forma que
+a variável A passe a possuir o valor de B e que a variável B passe a possuir o valor de A. Por
+fim, apresente estes valores trocados.
+*/
+
+  //antes
+  int a = 5;
+  int b = 10;
+  print('$a $b');
+  //depois
+  a = 10;
+  b = 5;
+  print('$a $b');
+}
